7e00118aefa1e7266cd2a3c7d86ec848f55a6eb1
okck <98981974+okck@users.noreply.github.com>
2022-05-12 21:20:48 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,8 +4,30 @@ for (x = 1; x < 101; x++) {
 document.getElementById('galleryImages').innerHTML += `<div class="gallery-img" id="galleryImage${x}"><img src="images/${x}.png" class="grid-photo" id="photo${x}" alt=""><div class="caption" id="caption${x}">${x}</div></div>`
 }
 
-for (y = 55; y < 101; y++) {
-  document.getElementById('swiperWrapper').innerHTML += `<div class="swiper-slide"><img src="images/${y}.png" class="hero-photo" alt=""></div>`
+// test
+const numbers = [];
+for (let i = 0; i < 100; i++){
+  numbers[i] = i + 1;
+}
+shuffle(numbers);
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+  
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+    array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+// test
+
+for (y = 1; y < 101; y++) {
+  // document.getElementById('swiperWrapper').innerHTML += `<div class="swiper-slide"><img src="images/${y}.png" class="hero-photo" alt=""></div>`
+
+  document.getElementById('swiperWrapper').innerHTML += `<div class="swiper-slide"><img src="images/${numbers[y]}.png" class="hero-photo" alt=""></div>`
   }
 
 document.querySelectorAll('.gallery-img').forEach(element => {
